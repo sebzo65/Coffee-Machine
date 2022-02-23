e8c9b104b7fac93a4ef28d00d0735ad8f7edbd51
Sebastian <sebzo65@gmail.com>
2022-02-23 11:10:41 +1100

    Completed 3rd stage of the project, which involved taking user input regarding amount of water, milk and coffee beans the machine has in total, as well as how many cups of coffee the user wants, and then outputting feedback to the user confirming whether this can be done (and how many cups extra or under can be made)

--- src/com/LearnJava/Main.java
+++ src/com/LearnJava/Main.java
@@ -6,9 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int water = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int beans = scanner.nextInt();
         System.out.println("Write how many cups of coffee you will need:");
         int cupsCoffee = scanner.nextInt();
-        CoffeeApp coffeeApp = new CoffeeApp(cupsCoffee);
-        coffeeApp.printIngredients();
+        CoffeeApp coffeeApp = new CoffeeApp(cupsCoffee, water, milk, beans);
+        coffeeApp.calculateCoffeeMaking();
     }
 }
